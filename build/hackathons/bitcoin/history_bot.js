e40ce4b3b90c1c492d697a92a0d2e1bbd3a461a9
William Farmer <willzfarmer@gmail.com>
2015-11-15 23:28:57 -0700

    Week 12

--- build/hackathons/bitcoin/history_bot.js
+++ build/hackathons/bitcoin/history_bot.js
@@ -1,34 +1,19 @@
-three_before = false
-two_before = false
-one_before = false
-
 function decideWhetherOrNotToTrade(tweet){
     keys = ['game', 'money', 'fun', 'good', 'actor', 'news', 'movies', 'tech', 'music', 'people', 'apple', 'google']
     pluskeys = ['game', 'news', 'movies', 'tech', 'music', 'people']
     negkeys = ['money', 'fun', 'good', 'actor', 'apple']
-
     var positive = (_.reduce(pluskeys, function(p, n) {
-        return p || tweet.includes(n)
+        return p || tweet.tweet.includes(n)
     }, false))
     var negative = (_.reduce(negkeys, function(p, n) {
-        return p || tweet.includes(n)
+        return p || tweet.tweet.includes(n)
     }, false))
 
-    var trade = false
-
     if ((!positive) && (!negative)) {
-        trade = false
+        return false
     } else if (bank.currency === 'USD') {
-        trade = (positive)?false:true
+        return (positive)?false:true
     } else {
-        trade = (positive)?true:false
+        return (positive)?true:false
     }
-
-    var buffer = three_before
-
-    three_before = two_before
-    two_before = one_before
-    one_before = trade
-
-    return buffer
 }
